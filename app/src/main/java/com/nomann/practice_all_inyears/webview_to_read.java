36d53109f1308acb7d2f462d5d493f529b5dbba3
sadidahammed2 <sadidahammed04@gmail.com>
2026-03-15 16:43:51 +0600

    3rd

--- app/src/main/java/com/nomann/practice_all_inyears/webview_to_read.java
+++ app/src/main/java/com/nomann/practice_all_inyears/webview_to_read.java
@@ -25,15 +25,12 @@ public class webview_to_read extends AppCompatActivity {
         webView = findViewById(R.id.webView);
         Button btnRead = findViewById(R.id.btnReadAndOpen);
 
-        // ১. WebView সেটিংস (JavaScript এনাবল করা জরুরি)
-        webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://www.thedailystar.net");
 
 
 
-        // ৩. বাটনে ক্লিক করলে ওয়েবসাইট থেকে টেক্সট স্ক্যান করবে
 
     }
 
